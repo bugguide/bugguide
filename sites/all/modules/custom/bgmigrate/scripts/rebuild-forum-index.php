fa23299cbf3cd2846409c83fdc3d7afaf1b3e02f
John VanDyk <jvandyk.git@sysarchitects.com>
2018-02-28 12:34:45 -0600

    BUGG-111 forum index needs rebuilding after node_access_statistics rebuild

---
+++ sites/all/modules/custom/bgmigrate/scripts/rebuild-forum-index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Script to rebuild data for forum topics in forum_index.
+ *
+ * @see forum_update_7001().
+ *
+ * @see bugguide_migration's migrateto7.
+ */
+ 
+$select = db_select('node', 'n');
+$forum_alias = $select
+  ->join('forum', 'f', 'n.vid = f.vid');
+$ncs_alias = $select
+  ->join('node_comment_statistics', 'ncs', 'n.nid = ncs.nid');
+$select
+  ->fields('n', array(
+  'nid',
+  'title',
+  'sticky',
+  'created',
+))
+  ->fields($forum_alias, array(
+  'tid',
+))
+  ->fields($ncs_alias, array(
+  'last_comment_timestamp',
+  'comment_count',
+));
+db_insert('forum_index')
+  ->fields(array(
+  'nid',
+  'title',
+  'sticky',
+  'created',
+  'tid',
+  'last_comment_timestamp',
+  'comment_count',
+))
+  ->from($select)
+  ->execute();
