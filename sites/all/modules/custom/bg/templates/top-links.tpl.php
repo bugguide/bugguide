09c60e3c8caf3c81940d23e9b578a6b7dfd9bdec
Juampy <juampynr@gmail.com>
2018-03-02 12:16:14 +0100

    BUGG-152 Add user links block

---
+++ sites/all/modules/custom/bg/templates/top-links.tpl.php
@@ -0,0 +1,7 @@
+<?php if ($logged_in): ?>
+    <ul>
+        <li><a href="/node/6">ID request</a></li>
+        <li><a href="/user">My account</a></li>
+        <li><a href="/user/logout">Logout</a></li>
+    </ul>
+<?php endif; ?>
