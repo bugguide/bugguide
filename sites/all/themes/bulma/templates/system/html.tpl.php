8f05993da3d611096055f335eecb035ce977d004
Carney, Sarah A <sacarney@iastate.edu>
2018-03-15 11:25:23 -0500

    BUGG-184 Used a variable to set language, instead of hardcoded

--- sites/all/themes/bulma/templates/system/html.tpl.php
+++ sites/all/themes/bulma/templates/system/html.tpl.php
@@ -39,14 +39,16 @@
  * @see template_preprocess_html()
  * @see template_process()
  */
-?><!DOCTYPE html>
-<html lang="en">
-  <head>
-    <?php print $head; ?>
-    <title><?php print $head_title; ?></title>
-    <?php print $styles; ?>
-    <?php print $scripts; ?>
-  </head>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+
+<head profile="<?php print $grddl_profile; ?>">
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
   <body class="<?php print $classes; ?>">
     <div id="skip-link">
       <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
