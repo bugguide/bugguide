d605602971e3c43f059f9c4400bc1696cbe96214
Carney, Sarah A <sacarney@iastate.edu>
2018-03-21 13:29:17 -0500

    BUGG-223 Create and place Visual Guide block

---
+++ sites/all/modules/custom/bg/templates/visual-guide.tpl.php
@@ -0,0 +1,147 @@
+<div class="bg-visual-guide">
+<nav aria-label="Visual Guide">
+  <div class="columns is-multiline is-mobile">
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/57" title="Moths">
+        <img src="/sites/all/themes/bulmabug/images/butterflies-moths-1.png" alt="Moths"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/81" title="Butterflies">
+        <img src="/sites/all/themes/bulmabug/images/butterflies-moths-2.png" alt="Butterflies"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/55" title="Flies">
+        <img src="/sites/all/themes/bulmabug/images/flies-1.png" alt="Flies"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/57" title="Caterpillars">
+        <img src="/sites/all/themes/bulmabug/images/butterflies-moths-3.png" alt="Caterpillars"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/55" title="Flies">
+        <img src="/sites/all/themes/bulmabug/images/flies-2.png" alt="Flies"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/191" title="Dragonflies">
+        <img src="/sites/all/themes/bulmabug/images/dragonflies.png" alt="Dragonflies"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/55" title="Flies">
+        <img src="/sites/all/themes/bulmabug/images/flies-3.png" alt="Flies"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/342391" title="Mantids">
+        <img src="/sites/all/themes/bulmabug/images/mantids.png" alt="Mantids"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/342386" title="Cockroaches">
+        <img src="/sites/all/themes/bulmabug/images/cockroaches-termites.png" alt="Cockroaches"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/59" title="Bees and Wasps">
+        <img src="/sites/all/themes/bulmabug/images/ants-bees-wasps-flies-1.png" alt="Bees and Wasps"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/59" title="Bees and Wasps">
+        <img src="/sites/all/themes/bulmabug/images/ants-bees-wasps-flies-2.png" alt="Bees and Wasps"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/59" title="Bees and Wasps">
+        <img src="/sites/all/themes/bulmabug/images/ants-bees-wasps-flies-3.png" alt="Bees and Wasps"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/74" title="Walkingsticks">
+        <img src="/sites/all/themes/bulmabug/images/walkingsticks.png" alt="Walkingsticks"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/2709" title="Earwigs">
+        <img src="/sites/all/themes/bulmabug/images/earwigs.png" alt="Earwigs"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/165" title="Ants">
+        <img src="/sites/all/themes/bulmabug/images/ants.png" alt="Ants"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/601964" title="Termites">
+        <img src="/sites/all/themes/bulmabug/images/termites.png" alt="Termites"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/63" title="Cicadas, Hoppers, and Kin">
+        <img src="/sites/all/themes/bulmabug/images/cicadas-hoppers-aphids-allies.png" alt="Cicadas, Hoppers, and Kin"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/63" title="Hoppers, and Kin">
+        <img src="/sites/all/themes/bulmabug/images/true-bugs-cicadas-hoppers-aphids-allies.png" alt="Hoppers, and Kin"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/60" title="Beetles">
+        <img src="/sites/all/themes/bulmabug/images/beetles-1.png" alt="Beetles"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/60" title="Beetles">
+        <img src="/sites/all/themes/bulmabug/images/beetles-2.png" alt="Beetles"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/94266" title="True Bugs">
+        <img src="/sites/all/themes/bulmabug/images/true-bugs.png" alt="True Bugs"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/7040" title="Fleas">
+        <img src="/sites/all/themes/bulmabug/images/fleas.png" alt="Fleas"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/73" title="Grasshoppers, Crickets, and Katydids">
+        <img src="/sites/all/themes/bulmabug/images/grasshoppers-crickets-katydids.png" alt="Grasshoppers, Crickets, and Katydids"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/91197" title="Ticks">
+        <img src="/sites/all/themes/bulmabug/images/mites-ticks.png" alt="Ticks"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/1954" title="Spiders">
+        <img src="/sites/all/themes/bulmabug/images/spiders.png" alt="Spiders"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+      <a href="/node/2441" title="Scorpions">
+        <img src="/sites/all/themes/bulmabug/images/scorpions.png" alt="Scorpions"></img>
+      </a>
+    </div>
+    <div class="column is-2-desktop is-2-tablet is-4-mobile">
+    <a href="/node/20" title="Centipedes">
+      <img src="/sites/all/themes/bulmabug/images/centipedes.png" alt="Centipedes"></img>
+    </a>
+  </div>
+  <div class="column is-2-desktop is-2-tablet is-4-mobile">
+    <a href="/node/37" title="Millipedes">
+      <img src="/sites/all/themes/bulmabug/images/millipedes.png" alt="Millipedes"></img>
+    </a>
+  </div>
+
+  </div>
+</nav>
+</div>
