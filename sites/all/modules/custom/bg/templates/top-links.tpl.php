8fb8b1ee7baaa9d10803b5a000938d044c53449e
Carney, Sarah A <sacarney@iastate.edu>
2018-03-23 11:02:25 -0500

    Added Bug Report button to anon as well

--- sites/all/modules/custom/bg/templates/top-links.tpl.php
+++ sites/all/modules/custom/bg/templates/top-links.tpl.php
@@ -28,6 +28,14 @@
 <?php else: ?>
   <ul class="is-flex">
     <li>
+      <a class="button is-tan mr-2" href="https://www.biology-it.iastate.edu/bugguide-bug-report">
+        <span class="icon">
+          <span class="fa fa-bug" aria-hidden="true"></span>
+        </span>
+        <span>Report Bug</span>
+      </a>
+    </li>
+    <li>
       <a class="button is-tan mr-2" href="/user">
         <span class="icon">
           <span class="fa fa-sign-in" aria-hidden="true"></span>
