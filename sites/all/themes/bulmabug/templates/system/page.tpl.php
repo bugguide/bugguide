d1258a4e89dfcc7531da6ade776cf450c820f880
John VanDyk <jvandyk@iastate.edu>
2020-07-05 20:59:40 -0500

    Warn that changes will be wiped on next import.

--- sites/all/themes/bulmabug/templates/system/page.tpl.php
+++ sites/all/themes/bulmabug/templates/system/page.tpl.php
@@ -223,7 +223,7 @@
       </div>
     </div>
     <?php if (gethostname() == 'bgbeta.ent.iastate.edu' && file_exists(DRUPAL_ROOT . '/sites/default/settings.php')) : ?>
-      <p>BugGuide 2.0 beta build completed <?php echo date("F d Y H:i", filemtime(DRUPAL_ROOT . '/sites/default/settings.php')) ?> CDT. (<?php echo substr($current_database, -1) ?>)</p>
+      <p>BugGuide 2.0 beta build completed <?php echo date("F d Y H:i", filemtime(DRUPAL_ROOT . '/sites/default/settings.php')) ?> CDT. (<?php echo substr($current_database, -1) ?>) <strong>Changes you make to this site will be overwritten by the next import (approximately every 24 hours).</strong></p>
     <?php endif;?>
   </footer>
 </div>
