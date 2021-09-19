e46ecb5c16dd27ce66dffce331f2cad3fc0eba5f
Tom Klein <twointofive@gmail.com>
2021-09-19 06:42:07 -0500

    #89 Wrap the node authoring data and add a bottom margin

--- sites/all/themes/bulmabug/templates/ds-1col--node-bglink.tpl.php
+++ sites/all/themes/bulmabug/templates/ds-1col--node-bglink.tpl.php
@@ -1,15 +1,19 @@
 <div class="content">
-	<div class="bg-link_title-link">
-		Visit: <a href="<?php print $variables['field_bglink_link'][0]['url']; ?>"><?php print $variables['field_bglink_link'][0]['url']; ?></a>
-	</div>
-	<?php print render($content['body']); ?>
+  <div class="bg-link_title-link">
+    Visit: <a href="<?php print $variables['field_bglink_link'][0]['url']; ?>"><?php print $variables['field_bglink_link'][0]['url']; ?></a>
+  </div>
+
+  <?php print render($content['body']); ?>
+
 </div>
 
-<div class="bg-link_byline is-size-7">
-  <span>Contributed by </span><?php print render($content['author']); ?><span> on </span><?php print render($content['post_date']); ?>
-</div>
-<div class="bg-link_updated is-size-7">
-	<span>Last updated </span><?php print render($content['changed_date']); ?>
+<div class="node-authoring-data">
+  <div class="bg-link_byline is-size-7">
+    <span>Contributed by </span><?php print render($content['author']); ?><span> on </span><?php print render($content['post_date']); ?>
+  </div>
+  <div class="bg-link_updated is-size-7">
+    <span>Last updated </span><?php print render($content['changed_date']); ?>
+  </div>
 </div>
 
 <?php print render($content['field_tags']); ?>
