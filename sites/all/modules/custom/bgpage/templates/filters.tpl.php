08c59c2d47909e5b8eb41384031a2cda1e64d7f2
Tom Klein <twointofive@gmail.com>
2021-11-14 14:52:54 -0600

    #24 Place the filters form behind a button.
    Bulma's navbar doesn't show a button when width > 1024, so we've had to override Bulma to do so.
    
    (I'm not sure if it's better to do it this way or to copy all of Bulma's navbar css into a new element(s) and then update the javascript to toggle the new element as well; this seems easier now, but I wonder if we could get broken by a Bulma update to navbar.)

--- sites/all/modules/custom/bgpage/templates/filters.tpl.php
+++ sites/all/modules/custom/bgpage/templates/filters.tpl.php
@@ -1,1 +1,10 @@
-<?php print drupal_render($filters_form); ?>
+<nav class="navbar filters-navbar">
+  <button class="button is-tan menu-toggle" data-target="FilterOptions" aria-pressed="false">
+    <span class="icon"><span class="fa fa-sliders" aria-hidden="true"></span></span>
+    <span>Filters</span>
+  </button>
+
+  <div id="FilterOptions" class="navbar-menu">
+    <?php print drupal_render($filters_form); ?>
+  </div>
+</nav>
