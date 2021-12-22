14ff7042c80924629cd5ca602df552f5ab7d5a25
John VanDyk <jvandyk@iastate.edu>
2021-12-22 11:07:27 -0600

    #7 use file_unmanaged_copy to avoid duplicate records

--- unique_image_hashes.php
+++ unique_image_hashes.php
@@ -26,61 +26,61 @@ function hashlog($s) {
 }
 
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-hashlog("booted up");
+//hashlog("booted up");
 // Walk through bgimage table.
-$result = db_query("SELECT nid, base FROM bgimage ORDER BY nid LIMIT 1");
+$result = db_query("SELECT nid, base, newbase FROM bgimage WHERE newbase IS NOT NULL ORDER BY nid");
 foreach ($result as $record) {
-    print_r($record);
-  // Generate a guaranteed-unique uuid.
-  $uuid = bgimage_generate_uuid();
-  //$uuid = $record->newbase;
+  //print_r($record);
+  $uuid = $record->newbase;
   
   // 1. Old filepath
   $old_filepath = image_obfuscate($record->base . $record->nid) . '.jpg';
-  hashlog("old_filepath: $old_filepath");
+  //hashlog("old_filepath: $old_filepath");
 
 
   // 2. New filepath
   $obfuscate = image_obfuscate($uuid . $record->nid);
-  hashlog("new_filepath: $obfuscate");
+  //hashlog("new_filepath: $obfuscate");
   
   // 3. If we have not already done so, copy the file.
   $prefix = bgimage_get_prefix($obfuscate);
   $directory = 'public://raw/' . $prefix;
-  hashlog ("new directory: $directory");
+  //hashlog ("new directory: $directory");
   // Ensure directory
   if (!file_prepare_directory($directory, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS)) {
     watchdog('image', 'Failed to create style directory: %directory', array('%directory' => $directory), WATCHDOG_ERROR);
-    hashlog( "aaaugh, could not create style directory $directory");
+    //hashlog( "aaaugh, could not create style directory $directory");
     return FALSE;
   }
 
   $uri = $directory . $obfuscate . '.jpg';
-  hashlog( "uri: $uri");
+  //hashlog( "uri: $uri");
   // File will exist if a previous migration already converted this file.
   $fid = db_query("SELECT fid FROM {file_usage} WHERE id = :nid", array(':nid' => $record->nid))->fetchField();
   $file = file_load($fid);
-  hashlog("Loaded file:");
-  print_r($file);
+  //hashlog("Loaded file:");
+  //print_r($file);
   // Save original uri; there is a file there.
   $raw_file_uri = $file->uri;
   if (!file_exists($uri)) {
-    // Munge the uri to point to raworig, an archive of files with current BugGuide paths
-    $file->uri = str_replace('raw', 'raworig', $file->uri);
     // Copy file into new path, e.g.,
     // files/raw/7H3/HEH/7H3HEHUZ6H3HMLUZ8LWZUHLRUHVZMLZR5LAZ8LUZ6HBH0L6Z4H6ZLLOHIHBHQL5Z8H.jpg
     
-    // TODO: file_copy is munging around with database entries, maybe we want to bypass.
-    file_copy($file, $directory . $obfuscate . '.jpg', FILE_EXISTS_ERROR); 
+    // TODO: file_copy() is munging around with database entries, maybe we want to bypass. Yes.
+    // If FILE_EXISTS_ERROR then it will create a new file entry?
+    //file_copy($file, $directory . $obfuscate . '.jpg', FILE_EXISTS_ERROR); 
+    file_unmanaged_copy($file->uri, $directory . $obfuscate . '.jpg', FILE_EXISTS_ERROR); 
     // 4. Update the uri in file_managed
     // but file_copy() does not update the filename so we have to do that
-    db_query("UPDATE {file_managed} SET filename = :filename WHERE fid = :fid", array(':filename' => $obfuscate . '.jpg', ':fid' => $fid));
+    db_query("UPDATE {file_managed} SET filename = :filename, uri = :uri WHERE fid = :fid", array(':filename' => $obfuscate . '.jpg', ':uri' => $uri, ':fid' => $fid));
+    $file_existed = 0;
   }
   else {
-      hashlog("file already exists");
+    //hashlog("file already exists");
+    $file_existed = 1;
   }
   // TODO delete $raw_file_url which is the original image path.
-  
+  // Deferring this.
   
   // 5. Update the hash in bgimage
   db_query("UPDATE {bgimage} SET base = :uuid WHERE nid = :nid", array(':uuid' => $uuid, ':nid' => $record->nid));
@@ -88,16 +88,20 @@ foreach ($result as $record) {
   // 6. Update the hash in field_data_bgimage_base
   db_query("UPDATE {field_data_field_bgimage_base} SET field_bgimage_base_value = :uuid WHERE entity_id = :nid", array(':uuid' => $uuid, ':nid' => $record->nid));
   
-  // 7. Update the has in field_revisions_bgimage_base
+  // 7. Update the hash in field_revisions_bgimage_base
   db_query("UPDATE {field_revision_field_bgimage_base} SET field_bgimage_base_value = :uuid WHERE entity_id = :nid", array(':uuid' => $uuid, ':nid' => $record->nid));
   
-  hashlog("");
+  hashlog("$file_existed $record->nid $record->base $uuid $old_filepath $obfuscate.jpg");
+  
+  continue;
   hashlog("$record->nid $record->base -> $uuid");
   hashlog("old: $old_filepath");
   hashlog("new: $directory$obfuscate.jpg");
   hashlog("");
   hashlog("Doing a check. Loading nid $record->nid");
-  // clear node cache
+  // Clear node cache by calling with reset TRUE.
+  // But we will get old base from cache_field. Zap it out.
+  cache_clear_all('field:node:' . $record->nid, 'cache_field');
   $node = node_load($record->nid, NULL, TRUE);
   hashlog("Base should be: $uuid");
   hashlog("Base is: " . $node->{'field_bgimage_base'}[LANGUAGE_NONE][0]['value']);
